7406645ae18990bfb852ecb8cf87c5e39933e22f
homoakin619 <homohaykins619@gmail.com>
2024-01-19 23:28:54 +0100

    added multilevel directory sending support

--- bin/receiver.js
+++ bin/receiver.js
@@ -2,13 +2,14 @@ const net = require("net");
 const fs = require("fs")
 const ora = require("ora")
 const chalk = require("chalk");
-const { saveFiles } = require("./utils");
+const { saveFiles, displayTitle } = require("./utils");
 
 
 let spinner;
 function receiveFile(IP_ADDRESS) {
     const client = net.connect({port: 3000,host:IP_ADDRESS},() => {
-        console.log(chalk.green.bold("🔗 Connected to sender\nHandshake Successful 🤝"));
+        displayTitle()
+        console.log(chalk.red.bold("🔗 Connected to sender\nHandshake Successful 🤝"));
         client.write("Handshake completed")
         spinner = ora("Receiving file data").start();
     })
@@ -16,8 +17,7 @@ function receiveFile(IP_ADDRESS) {
     let incomingFile = Buffer.alloc(0);
     client.on("data",(data) => {
         incomingFile = Buffer.concat([incomingFile,data]);
-        // console.log("Data received");
-        spinner.text = chalk.bold.cyan(`Receiving file data (${incomingFile.length} bytes)`)
+        spinner.text = chalk.bold.cyan(`Receiving file: (${incomingFile.length} bytes)`)
     })
 
     client.on("error",(error) => {
@@ -27,13 +27,8 @@ function receiveFile(IP_ADDRESS) {
 
     client.on('end',() => {
         const payload = JSON.parse(incomingFile.toString())
-        console.log(payload);
         saveFiles(payload)
-        // let content =  Buffer.from(payload.file.data);
-        // fs.writeFileSync(payload.filename,content);
-        spinner.succeed(chalk.bold.green(`File received and saved successfully: ${payload.filename}`));
-        // console.log("File saved successfully");
-        // client.end()
+        spinner.succeed(chalk.bold.green(`File received and saved successfully 🚀`));
     })
     client.on("close",() => {
         console.log("client has been closed");
